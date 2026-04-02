79d8f090161a5d74e7c57c8a28d9c58edd7196e3
gyuksel25 <gyuksel25@ku.edu.tr>
2026-04-03 02:44:21 +0300

    prelab05

--- q1.py
+++ q1.py
@@ -1,5 +1,25 @@
+def is_palindrome(string):
+    left=0
+    right=len(string)-1
+    while left<right:
+        if string[left]!=string[right]:
+            return False
+        left+=1
+        right-=1
+    return True
 def longest_palindromic_substring(s):
     """
     Given a string find the longest palindromic substring
     """
-    pass
+    longest=""
+    i=0
+    while i<len(s):
+        j=i+2
+        while j<=len(s):
+            string=s[i:j]
+            if is_palindrome(string):
+                if len(string)>len(longest):
+                    longest =string
+            j+=1
+        i+=1
+    return longest
--- q2.py
+++ q2.py
@@ -2,4 +2,12 @@ def remove_adjacent_duplicates(s):
     '''
     Given a string remove all the adjacent duplicate characters and return the string
     '''
-    pass
+    result=""
+    i=0
+    while i<len(s):
+        if len(result)>0 and result[len(result)-1]==s[i]:
+            result=result[:len(result)-1]
+        else:
+            result+=s[i]
+        i+=1
+    return result
